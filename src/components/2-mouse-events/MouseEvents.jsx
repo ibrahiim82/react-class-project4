990aaa8bc9a989643e57163042716b146a4ed57b
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-08-28 22:51:58 +0300

    lastcommit

--- src/components/2-mouse-events/MouseEvents.jsx
+++ src/components/2-mouse-events/MouseEvents.jsx
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 
+//* Mouseover olayı, fare işaretçisi div öğesine ve onun alt öğelerine girdiğinde tetiklenir.
+
+//* Mouseenter olayı yalnızca fare işaretçisi div öğesine girdiğinde tetiklenir.
+
+//* Onmousemove olayı, fare işaretçisi div öğesinin üzerinde her hareket ettiğinde tetiklenir.
+
+//? pageX/Ykoordinatlar, oluşturulan sayfanın tamamının sol üst köşesine göredir (kaydırılarak gizlenen parçalar dahil),
+
+//? clientX/YKoordinatlar sayfanın görünen kısmının sol üst köşesine göredir ve tarayıcı penceresinden "görünür" .
+
+//? screenX/Yfiziksel ekrana göredir.
+
 const MouseEvents = () => {
   const [visible, setVisible] = useState(false);
+  const [coordX, setCoordX] = useState("");
+  const [coordY, setCoordY] = useState("");
+
   const handleDoubleClick = (e) => e.target.classList.toggle("bg-danger");
 
   const handleMouseMove = (e) => {
-    //  console.log(e);
+    //? mutlak koordinatlar
+    // console.log("X:", e.pageX);
+    // console.log("Y:", e.pageY);
+    // console.log("X:", e.clientX);
+    // console.log("Y:", e.clientY);
+
+    //? göreceli koordinatlar
+    setCoordX(e.nativeEvent.offsetX);
+    setCoordY(e.nativeEvent.offsetY);
   };
 
   return (
@@ -19,8 +42,7 @@ const MouseEvents = () => {
       >
         onMouseOver & onMouseLeave
       </div>
-      {visible && <p className="text-danger">Mouse Üzerinde</p>}
-
+      {visible && <p className="text-danger">Mouse üzerimde</p>}
       <div
         onDoubleClick={handleDoubleClick}
         id="todo-2"
@@ -28,15 +50,28 @@ const MouseEvents = () => {
       >
         onDoubleClick
       </div>
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        onMouseMove={handleMouseMove}
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+      >
         <span>onMouseMove</span>
       </div>
-      <div id="todo-4" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        onMouseOver={handleMouseMove}
+        id="todo-4"
+        className="bg-success text-light w-50 p-4 my-4"
+      >
         <span>onMouseOver</span>
       </div>
-      <div id="todo-5" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        onMouseEnter={handleMouseMove}
+        id="todo-5"
+        className="bg-success text-light w-50 p-4 my-4"
+      >
         <span>onMouseEnter</span>
       </div>
+      X: {coordX} and Y:{coordY}
     </div>
   );
 };
