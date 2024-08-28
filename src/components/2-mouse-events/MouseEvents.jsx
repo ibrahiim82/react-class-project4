682f42ee40b7c75f985d4c618c58eba052f02a95
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-08-28 22:27:19 +0300

    lastcommit

--- src/components/2-mouse-events/MouseEvents.jsx
+++ src/components/2-mouse-events/MouseEvents.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MouseEvents = () => {
+  const [visible, setVisible] = useState(false);
+  const handleDoubleClick = (e) => e.target.classList.toggle("bg-danger");
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
-      <div id="todo-1" className="bg-success text-light w-50 p-4">
+      <div
+        onMouseOver={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+      >
         onMouseOver & onMouseLeave
       </div>
+      {visible && <p className="text-danger">Mouse Üzerinde</p>}
 
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+      <div
+        onDoubleClick={handleDoubleClick}
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+      >
         onDoubleClick
       </div>
       <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
